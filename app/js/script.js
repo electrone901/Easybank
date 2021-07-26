c99258a7ae3109048f6bae996023912762882bf9
electrone901 <carbajalluis@rocketmail.com>
2021-07-26 02:11:21 -0400

    Added: animations using keyframes and visibity hidden (display none doesnt work with animations)

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
 
 btnHamburger.addEventListener('click', () => {
   console.log('open')
   if (header.classList.contains('open')) {
+    // when close
     header.classList.remove('open')
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
   } else {
+    //when open
     header.classList.add('open')
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('fade-in')
   }
 })
