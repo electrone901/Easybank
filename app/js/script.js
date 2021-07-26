93ca027aa4e1a46de12d2208e18c93c699cd45c6
electrone901 <carbajalluis@rocketmail.com>
2021-07-26 01:49:52 -0400

    Added: keyFrames animation to the hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+
+btnHamburger.addEventListener('click', () => {
+  console.log('open')
+  if (header.classList.contains('open')) {
+    header.classList.remove('open')
+  } else {
+    header.classList.add('open')
+  }
+})
